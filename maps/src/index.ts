89d19d099d72396c01be7ea7f19cf76cc1c903a4
Qassim Alshakhoori <qalshakhoori@moic.gov.bh>
2021-04-15 09:31:56 +0300

    add compnay and google maps

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,5 +1,18 @@
-import { User } from './User';
+/// <reference types="@types/googlemaps" />
 
-const user = new User();
+// import { User } from './User';
+// import { Company } from './Company';
 
-console.log(user);
+// const user = new User();
+
+// console.log(user);
+
+// const company = new Company();
+
+new google.maps.Map(document.getElementById('map'), {
+  zoom: 1,
+  center: {
+    lat: 0,
+    lng: 0,
+  },
+});

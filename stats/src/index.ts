a422c82ac3dc2c29fd712dd47e5bab17cc106266
Qassim Alshakhoori <qalshakhoori@moic.gov.bh>
2021-04-28 09:31:25 +0300

    stats project

---
+++ stats/src/index.ts
@@ -0,0 +1,24 @@
+import fs from 'fs';
+
+const matches = fs
+  .readFileSync('football.csv', { encoding: 'utf-8' })
+  .split('\n')
+  .map((row: string): string[] => {
+    return row.split(',');
+  });
+
+const homeWin = 'H';
+const awayWin = 'A';
+const draw = 'D';
+
+let manUnitedWins = 0;
+
+for (const match of matches) {
+  if (match[1] === 'Man United' && match[5] === homeWin) {
+    manUnitedWins++;
+  } else if (match[2] === 'Man United' && match[5] === awayWin) {
+    manUnitedWins++;
+  }
+}
+
+console.log(manUnitedWins);

43025fa684c4e0e3f37a98ae3adfa0ea1f0dd66e
Qassim Alshakhoori <qalshakhoori@moic.gov.bh>
2021-04-29 09:12:53 +0300

    sorter

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,22 +1,22 @@
-import fs from 'fs';
+import { CsvFileReader } from './CsvFileReader';
 
-const matches = fs
-  .readFileSync('football.csv', { encoding: 'utf-8' })
-  .split('\n')
-  .map((row: string): string[] => {
-    return row.split(',');
-  });
+const reader = new CsvFileReader('football.csv');
+reader.read();
 
-const homeWin = 'H';
-const awayWin = 'A';
-const draw = 'D';
+console.log(reader.data);
+
+enum MatchResults {
+  HomeWin = 'H',
+  AwayWin = 'A',
+  Draw = 'D',
+}
 
 let manUnitedWins = 0;
 
-for (const match of matches) {
-  if (match[1] === 'Man United' && match[5] === homeWin) {
+for (const match of reader.data) {
+  if (match[1] === 'Man United' && match[5] === MatchResults.HomeWin) {
     manUnitedWins++;
-  } else if (match[2] === 'Man United' && match[5] === awayWin) {
+  } else if (match[2] === 'Man United' && match[5] === MatchResults.AwayWin) {
     manUnitedWins++;
   }
 }

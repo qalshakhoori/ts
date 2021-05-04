4d6ff70f56843e5a856675afe0db79aef503cff6
Qassim Alshakhoori <qassim.alshakhoori@gmail.com>
2021-05-04 13:25:08 +0300

    CsvFileReader with interface

--- stats/src/MatchReader.ts
+++ stats/src/MatchReader.ts
@@ -1,19 +1,32 @@
-import { CsvFileReader } from './CsvFileReader';
 import { MatchResults } from './MatchResults';
 import { dateStringToDate } from './utils';
 
 type MatchData = [Date, string, string, number, number, MatchResults, string];
 
-export class MatchReader extends CsvFileReader<MatchData> {
-  mapRow(row: string[]): MatchData {
-    return [
-      dateStringToDate(row[0]),
-      row[1],
-      row[2],
-      parseInt(row[3]),
-      parseInt(row[4]),
-      row[5] as MatchResults,
-      row[6],
-    ];
+interface DataReader {
+  read(): void;
+  data: string[][];
+}
+
+export class MatchReader {
+  matchs: MatchData[] = [];
+
+  constructor(public reader: DataReader) {}
+
+  load(): void {
+    this.reader.read();
+    this.matchs = this.reader.data.map(
+      (row: string[]): MatchData => {
+        return [
+          dateStringToDate(row[0]),
+          row[1],
+          row[2],
+          parseInt(row[3]),
+          parseInt(row[4]),
+          row[5] as MatchResults,
+          row[6],
+        ];
+      }
+    );
   }
 }
--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,14 +1,18 @@
 import { MatchReader } from './MatchReader';
+import { CsvFileReader } from './CsvFileReader';
 import { MatchResults } from './MatchResults';
 
-const reader = new MatchReader('football.csv');
-reader.read();
+// Create an object that satisfies the 'DataReader' interface
+const csvFileReader = new CsvFileReader('football.csv');
 
-console.log(reader.data);
+// Create an instance of MatchReader and pass in something satifying
+// the 'DataReader' interface
+const macthReader = new MatchReader(csvFileReader);
+macthReader.load();
 
 let manUnitedWins = 0;
 
-for (const match of reader.data) {
+for (const match of macthReader.matchs) {
   if (match[1] === 'Man United' && match[5] === MatchResults.HomeWin) {
     manUnitedWins++;
   } else if (match[2] === 'Man United' && match[5] === MatchResults.AwayWin) {

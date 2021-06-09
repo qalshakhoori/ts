e886e829caf16299dccd3a7eebff5c8030a3b952
Qassim Alshakhoori <qalshakhoori@moic.gov.bh>
2021-06-09 08:58:09 +0300

    decoratores

---
+++ features/decorators.ts
@@ -0,0 +1,55 @@
+@classDecorator
+class Boat {
+  @testDecorator
+  color: string = 'red';
+
+  @testDecorator
+  get formattedColor(): string {
+    return `This boats color is ${this.color}`;
+  }
+
+  @logError('Oops boat was sun in the ocean')
+  pilot(
+    @parameterDecorator speed: string,
+    @parameterDecorator generateWake: boolean
+  ): void {
+    if (speed === 'fast') {
+      console.log('swish');
+    } else {
+      console.log('nothing');
+    }
+  }
+}
+
+function classDecorator(constructor: typeof Boat) {
+  console.log(constructor);
+}
+
+function parameterDecorator(target: any, key: string, index: number) {
+  console.log(key, index);
+}
+
+function testDecorator(target: any, key: string) {
+  console.log(key);
+}
+
+function logError(errorMessage: string) {
+  return function (target: any, key: any, desc: PropertyDescriptor): void {
+    const method = desc.value;
+
+    desc.value = function () {
+      try {
+        method();
+      } catch (error) {
+        console.log(errorMessage);
+      }
+    };
+  };
+}
+
+const boat = new Boat();
+boat.pilot('fast', true);
+// console.log(Object.getOwnPropertyDescriptor(boat, 'color'));
+// Object.defineProperty(boat, 'color', { writable: true });
+// boat.color = 'white';
+// console.log(boat);

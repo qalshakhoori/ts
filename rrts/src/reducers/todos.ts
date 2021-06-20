d68fca31a46adbb2ae08ca01a305381ed177a88f
Qassim Alshakhoori <qalshakhoori@moic.gov.bh>
2021-06-20 11:36:20 +0300

    react redux actions with typescript

---
+++ rrts/src/reducers/todos.ts
@@ -0,0 +1,10 @@
+import { Todo, ActionTypes, Action } from '../actions';
+
+export const todosReducer = (state: Todo[] = [], action: Action) => {
+  switch (action.type) {
+    case ActionTypes.fetchTodos:
+      return action.payload;
+    default:
+      return state;
+  }
+};
